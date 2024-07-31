8158f411ab2aadffae3e4cb1e9feb5d380b7209b
Brian Mosbeux <b.mosbeux@gmail.com>
2024-08-01 00:07:49 +0200

    Add muzzle_flash to weapon
    Add muzzleflash sprites
    Create muzzle_flash animation
    play muzzle_flash animation on shoot()

--- scripts/weapon.gd
+++ scripts/weapon.gd
@@ -9,6 +9,7 @@ signal weapon_fired(bullet, bullet_start_postion, bullet_direction)
 @onready var end_of_gun = $EndOfGun
 @onready var gun_direction = $GunDirection
 @onready var weapon_cooldown = $WeaponCooldown
+@onready var animation_player = $AnimationPlayer
 
 
 var dead_zone: float = .9
@@ -33,7 +34,8 @@ func rotate_weapon(delta):
 
 func shoot():
 	if weapon_cooldown.is_stopped():
-		weapon_cooldown.start()
 		var bullet_instance = BulletScene.instantiate()
 		var bullet_direction = (gun_direction.global_position - end_of_gun.global_position).normalized()
 		weapon_fired.emit(bullet_instance, end_of_gun.global_position, bullet_direction)
+		weapon_cooldown.start()
+		animation_player.play("muzzle_flash")
