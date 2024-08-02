f115fd9e8ae390dc395bc54854d536812efef249
Brian Mosbeux <b.mosbeux@gmail.com>
2024-08-02 13:03:11 +0200

    Tweak enemy ai
    fix patrol state: default = -1, on ready: State.PATROL
    enemy rotates first, then move toward patrol location

--- scripts/ai.gd
+++ scripts/ai.gd
@@ -14,14 +14,17 @@ enum State {
 @onready var patrol_timer = $PatrolTimer
 
 
-var current_state: int = State.PATROL:
+var current_state: int = -1:
 	set(new_state):
 		if new_state == current_state:
 			return
 		if new_state == State.PATROL:
-			origin = enemy.global_position
+			origin = global_position
 			patrol_timer.start()
 			patrol_location_reached = true
+		
+		else:
+			print("no state detected")
 		current_state = new_state
 		state_changed.emit(current_state)
 var player: Player = null
@@ -34,15 +37,19 @@ var patrol_location_reached: bool = false
 var enemy_velocity: Vector2 = Vector2.ZERO
 
 
+func _ready():
+	current_state = State.PATROL
+
 func _process(delta):
 	match current_state:
 		State.PATROL:
 			if not patrol_location_reached:
 				var angle_to_patrol_location: float = (patrol_location - enemy.global_position).angle()
 				enemy.rotation = rotate_toward(enemy.global_rotation, angle_to_patrol_location, 2 * delta)
-				enemy.velocity = Vector2(enemy.speed, 0.0).rotated(angle_to_patrol_location)
-				enemy.position += enemy.velocity * delta
-				enemy.move_and_slide()
+				if angle_to_patrol_location == enemy.rotation:
+					enemy.velocity = Vector2(enemy.speed, 0.0).rotated(angle_to_patrol_location)
+					enemy.position += enemy.velocity * delta
+					enemy.move_and_slide()
 				if enemy.global_position.distance_to(patrol_location) < 5:
 					patrol_location_reached = true
 					enemy.velocity = Vector2.ZERO
@@ -50,7 +57,6 @@ func _process(delta):
 		State.ENGAGE:
 			if player and weapon:
 				var angle_to_player = (player.global_position - enemy.global_position).angle()
-				print(angle_to_player)
 				weapon.global_rotation = rotate_toward(weapon.global_rotation, angle_to_player, 2 * delta)
 				if abs(weapon.global_rotation - angle_to_player) < 0.1:
 					weapon.shoot()
@@ -77,7 +83,7 @@ func _on_detection_zone_body_exited(body):
 
 
 func _on_patrol_timer_timeout():
-	var patrol_range: int = 50
+	var patrol_range: int = 150
 	var random_x = randi_range(-patrol_range, patrol_range)
 	var random_y = randi_range(-patrol_range, patrol_range)
 	patrol_location = Vector2(random_x, random_y) + origin
