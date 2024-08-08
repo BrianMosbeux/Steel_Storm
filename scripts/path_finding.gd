e6d2ebeaf79a5e3418e99d61f17b3f9d7af687c2
Brian Mosbeux <b.mosbeux@gmail.com>
2024-08-08 19:25:18 +0200

    Create a grid of ColorRect in PathFinding scene to check if a Astargrid2s point is set a solid or not (astar_grid.set_point_solid())
    Loop through the tilemap used_cells of layer 0 and draw a rectangle in each tile position
    Each colorrect is then referenced in a grid_rect dict variable. dict keys are the map position of each tile in the tilemap (0,0; 0,1; 0,2, ...)
    Each the the update_navigation_map() function is called (in _physic_process):
    Loop through the grid_rect dict and set the corresponding astargrid point as taversible (solid = false)
    also set each grid_rect (grid_rects[i]) color as enable
    Loop through all the nodes in group obstacle and do the oposite

--- scripts/path_finding.gd
+++ scripts/path_finding.gd
@@ -1,29 +1,51 @@
 extends Node2D
 class_name PathFinding
 
+
+@export var enabled_color: Color = Color("0000ff55")
+@export var disabled_color: Color = Color("c11f3658")
+@export var should_display_grid: bool = false
+
+var grid_rects := {}
 var tile_map: TileMap
 var astar_grid = AStarGrid2D.new()
 var half_cell_size: Vector2
 
+
+@onready var grid = $Grid
+
+
 func _physics_process(delta):
 	update_navigation_map()
-
 
 func create_astar_grid_2d(tile_map: TileMap):
 	self.tile_map = tile_map
 	astar_grid.region = tile_map.get_used_rect()
+	print(astar_grid.region)
 	#print(astar_grid.region)
 	astar_grid.cell_size = tile_map.tile_set.tile_size
 	half_cell_size = astar_grid.cell_size / 2
 	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_ONLY_IF_NO_OBSTACLES
 	astar_grid.update()
-	
-	
-			#print(astar_grid.get_point_position(Vector2i(x,y)))
+	var tiles = tile_map.get_used_cells(0)
+	if should_display_grid:
+		for tile in tiles:
+			var rect = ColorRect.new()
+			grid.add_child(rect)
+			grid_rects[tile] = rect
+			rect.modulate = Color(1, 1, 1, 0.5)
+			rect.color = enabled_color
+			rect.mouse_filter = Control.MOUSE_FILTER_IGNORE
+			rect.size = astar_grid.cell_size
+			rect.position = tile_map.map_to_local(tile) - half_cell_size
 
 func update_navigation_map():
-	create_astar_grid_2d(tile_map)
+	#create_astar_grid_2d(tile_map)
 	var region_size = astar_grid.region.size
+	for i in grid_rects:
+		astar_grid.set_point_solid(i, false)
+		if should_display_grid:
+			grid_rects[i].color = enabled_color
 	var obstacles = get_tree().get_nodes_in_group("obstacles")
 	#print(obstacles)
 	#for x in region_size.x:
@@ -36,6 +58,10 @@ func update_navigation_map():
 		if obstacle is CharacterBody2D:
 			var tile_coord = tile_map.local_to_map(obstacle.collision_shape_2d.global_position)
 			astar_grid.set_point_solid(tile_coord, true)
+			if should_display_grid:
+				grid_rects[tile_coord].color = disabled_color
+			#draw_rect(Rect2i(tile_map.map_to_local(tile_coord), astar_grid.cell_size), disabled_color, true)
+			
 	
 	
 				
